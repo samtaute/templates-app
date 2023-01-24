1e04943fcd5d7c35916a3f346e797df904ee65a1
sam-taute <sam.taute@digitalturbine.com>
2023-01-24 11:07:17 -0500

    Update import/page template

--- src/components/ImportField.vue
+++ src/components/ImportField.vue
@@ -12,8 +12,8 @@
                 <div class="accordion-body">
                     <form @submit.prevent="submitUserJson">
                         <div class="mb-3">
-                            <label for="exampleFormControlTextarea1" class="form-label">Copy and Paste Page JSON</label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <label for="userInput" class="form-label">Copy and Paste Page JSON</label>
+                            <textarea class="form-control" id="userInput" rows="3" v-model="userInput"></textarea>
                             <button class="btn btn-primary">Generate Page</button>
                         </div>
                     </form>
@@ -45,13 +45,430 @@
 <script>
 
 export default {
+    data() {
+        return {
+            userInput: ""
+        }
+    },
     methods: {
         submitUserJson() {
             console.log("submit")
+            this.$store.dispatch('processJson',this.userInput)
             this.$emit('generate-page', this.userInput)
         }
     },
+    beforeMount() {
+        const sampleInput = `{
+  "title": "Trending at 3pm",
+  "filename": "1500",
+  "view": "one_column",
+  "language": "en",
+  "outputTargets": ["content"],
+  "iab_categories": ["News"],
+  "platformsIncludeAllByProduct": ["firstscreen"],
+  "categories": ["news"],
+  "campaigns": {
+    "default": {
+      "type": "single",
+      "config": "default"
+    },
+    "custom": [
+      {
+        "type": "useragent",
+        "platforms": ["metropcs"],
+        "matchList": [
+          "Moto G Stylus",
+          "LM-K500",
+          "SM-A025U",
+          "SM-A125U",
+          "SM-A326U",
+          "LM-Q730",
+          "SM-A205U",
+          "SM-A215U",
+          "5007Z",
+          "Moto G Play",
+          "SM-J260T1",
+          "LM-K300"
+        ],
+        "config": "useragent"
+      },
+      {
+        "type": "useragent",
+        "platforms": ["boost"],
+        "matchList": [
+          "LM-X220PM",
+          "LM-Q720"
+      ],
+        "config": "useragent"
+      },
+      {
+        "type": "mode",
+        "platforms": ["boost", "cricket"],
+        "filter": {
+          "twa": "twa",
+          "default": "default"
+        }
+      }
+    ]
+  },
+  "section_navigation_bar": {
+    "display": true,
+    "platformsBlacklist": ["metropcs"]
+  },
+  "notification": {
+    "description": "Check out these trending photo galleries.",
+    "title": "Hit the Afternoon Slump?"
+  },
+  "blocks": [
+    {
+      "blockType": "playbuzz_video_block",
+      "platforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "ad_unit",
+      "platforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "outbrain_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_9",
+        "layout": "edge"
+      }
+    },
+    {
+      "blockType": "ad_unit",
+      "platforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "fotoscape_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "layout": "small-photocard",
+        "count": 6
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_6",
+        "layout": "edge"
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_6",
+        "layout": "tile"
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_6",
+        "layout": "edge"
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "platforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_6",
+        "layout": "edge"
+      }
+    },
+    {
+      "blockType": "ad_unit",
+      "platforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "header_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "subheader": "News & Gossip"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "layout": "edge-description",
+        "count": 1,
+        "category": "stereogum",
+        "link_destination": "referral"
+      }
+    },
+    {
+      "blockType": "ad_unit",
+      "excludePlatforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "carousel",
+        "count": 4,
+        "category": "celebrities",
+        "link_destination": "referral"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "category": "celebrities",
+        "count": 4,
+        "layout": "tile"
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "platforms": ["blu"],
+      "settings": {
+        "feeds": [{ "feed_id": "fotoscape_all_en" }],
+        "layout": "small-photocard",
+        "count": 5
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "excludePlatforms": ["metropcs", "blu", "boost"],
+      "settings": {
+        "feeds": [{ "feed_id": "fotoscape_all_en" }],
+        "layout": "slideshow",
+        "count": 5
+      }
+    },
+    {
+      "blockType": "quotes_widget_block",
+      "excludePlatforms": ["metropcs", "boost"]
+    },
+    {
+      "blockType": "header_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "subheader": "Top Headlines"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "layout": "edge-description",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "excludePlatforms": ["metropcs", "boost"],
+      "settings": {
+        "widgetId": "JS_6",
+        "layout": "list-medium"
+      }
+    },
+    {
+      "blockType": "header_block",
+      "settings": {
+        "subheader": "Explore More"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "edge-description",
+        "count": 3
+      }
+    },
+    {
+      "blockType": "memes_widget_block"
+    },
+    {
+      "blockType": "ad_unit"
+    },
+    {
+      "blockType": "outbrain_widget_block",
+      "platforms": ["boost"],
+      "settings": {
+        "widgetId": "REELM_1"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "category": "mix",
+        "layout": "carousel",
+        "count": 5
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "tile",
+        "count": 4,
+        "category": "licensed-news"
+      }
+    },
+    {
+      "blockType": "horoscope_carousel_block",
+      "settings": {
+        "type": "general"
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "carousel",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "settings": {
+        "widgetId": "JS_9",
+        "layout": "list-medium"
+      }
+    },
+    {
+      "blockType": "jokes_widget_block"
+    },
+    {
+      "blockType": "ad_unit"
+    },
+    {
+      "blockType": "header_block",
+      "settings": {
+        "subheader": "Play a Quick Game"
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "arkadium_arena_en" }],
+        "layout": "games-hero",
+        "count": 1
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "arkadium_arena_en" }],
+        "layout": "games-tile",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "playbuzz_superstar_en" }],
+        "layout": "quiz-hero",
+        "count": 1
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "playbuzz_superstar_en" }],
+        "layout": "quiz-tile",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "edge-description",
+        "count": 3
+      }
+    },
+    {
+      "blockType": "fortune_cookie_block"
+    },
+    {
+      "blockType": "ad_unit"
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "tile",
+        "count": 4,
+        "category": "licensed-news"
+      }
+    },
+    {
+      "blockType": "quotes_widget_block"
+    },
+    {
+      "blockType": "fotoscape_block",
+      "settings": {
+        "layout": "carousel",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "settings": {
+        "widgetId": "JS_9",
+        "layout": "list-medium"
+      }
+    },
+    {
+      "blockType": "fortune_cookie_block"
+    },
+    {
+      "blockType": "header_block",
+      "settings": {
+        "subheader": "Play a Quick Game"
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "arkadium_arena_en" }],
+        "layout": "games-hero",
+        "count": 1
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "arkadium_arena_en" }],
+        "layout": "games-tile",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "playbuzz_superstar_en" }],
+        "layout": "quiz-hero",
+        "count": 1
+      }
+    },
+    {
+      "blockType": "rss_feed_block",
+      "settings": {
+        "feeds": [{ "feed_id": "playbuzz_superstar_en" }],
+        "layout": "quiz-tile",
+        "count": 4
+      }
+    },
+    {
+      "blockType": "outbrain_block",
+      "settings": {
+        "widgetId": "JS_5",
+        "layout": "list-medium"
+      }
+    },
+    {
+      "blockType": "ad_unit"
+    }
+  ]
+}`
 
+this.userInput=sampleInput; 
+
+    }
 }
 
 </script>
--- src/components/PageTemplate.vue
+++ src/components/PageTemplate.vue
@@ -1,8 +1,46 @@
 <template>
-    <div id="page-template-container">Page Template Container</div>
+    <div id="page-template-container">
+        <draggable :list="$store.getters.blocks" :disabled="!enabled" item-key="name"
+            class="list-group layout-container" ghost-class="ghost" :move="checkMove" @start="dragging = true"
+            @end="dragging = false">
+            <template #item="{ element, index }">
+                <div class="list-group-item" :class="{ 'not-draggable': !enabled }" :element="element" :index="index">
+                    {{ element }}
+                </div>
+            </template>
+
+        </draggable>
+
+
+    </div>
+
 </template>
 <script>
+import draggable from 'vuedraggable'
+export default {
+    components: {
+        draggable
+    },
+    data() {
+        return {
+            dragging: false,
+            enabled: true,
+        };
+    },
+    methods: {
+        checkMove: function (e) {
+            window.console.log("Future index: " + e.draggedContext.futureIndex);
+        },
+    },
+    computed: {
+        draggingInfo() {
+            return this.dragging ? "under drag" : "";
+        }
+    },
 
+
+
+}
 </script>
 <style>
 #page-template-container {
