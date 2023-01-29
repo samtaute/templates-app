aeb77b4442fc5c63fcf81898944b1d97cf0483d1
sam-taute <sam.taute@digitalturbine.com>
2023-01-29 13:14:22 -0500

    update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:'templates-app'
+  publicPath:'/templates-app/'
 })
