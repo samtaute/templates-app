e797ecd621a2bf4bc6cb3b14d2386f6679f39c80
sam-taute <sam.taute@digitalturbine.com>
2023-02-06 12:59:23 -0500

    ad unit

---
+++ src/components/PreviewAdUnit.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="ad-unit-container">
+        <div class="ad-unit">
+
+        </div>
+    </section>
+
+
+</template>
+
+<script>
+
+
+</script>
+
+<style>
+.ad-unit {
+    height: 250px;
+    width: 300px;
+    border: 1px solid black;
+
+}
+
+.ad-unit-container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+}
+</style>
--- src/components/PreviewBlock.vue
+++ src/components/PreviewBlock.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <preview-block-header v-if="element.blockType === 'header_block'" :element='element' :key="element.blockType"></preview-block-header> 
         <preview-ad-unit  v-if="element.blockType === 'ad_unit'" :element='element' :key="element.blockType"></preview-ad-unit>
         <fotoscape-block v-if="element.blockType==='fotoscape_block'" :element='element' :key="element.blockType"></fotoscape-block>
     </div>
@@ -9,14 +8,12 @@
 </template>
 <script>
 import FotoscapeBlock from './FotoscapeBlock.vue'
-import PreviewBlockHeader from './PreviewBlockHeader.vue'
 import PreviewAdUnit from './PreviewAdUnit.vue'
 
 export default{
     props: ['element', 'fotoscapeObj'],
     components:{
         FotoscapeBlock,
-        PreviewBlockHeader,
         PreviewAdUnit,
     }
 }
