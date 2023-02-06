7d64bca9fad41b56c6aec4197db08d04fea5567e
sam-taute <sam.taute@digitalturbine.com>
2023-02-06 12:58:48 -0500

    Create ad unit and fotoscape block

--- src/components/PreviewBlock.vue
+++ src/components/PreviewBlock.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
-        
-        <fotoscape-block v-if="element.blockType==='fotoscape_block'" :count="element.settings.count" :key="element.blockType"></fotoscape-block>
+        <preview-block-header v-if="element.blockType === 'header_block'" :element='element' :key="element.blockType"></preview-block-header> 
+        <preview-ad-unit  v-if="element.blockType === 'ad_unit'" :element='element' :key="element.blockType"></preview-ad-unit>
+        <fotoscape-block v-if="element.blockType==='fotoscape_block'" :element='element' :key="element.blockType"></fotoscape-block>
     </div>
    
 
 </template>
 <script>
 import FotoscapeBlock from './FotoscapeBlock.vue'
+import PreviewBlockHeader from './PreviewBlockHeader.vue'
+import PreviewAdUnit from './PreviewAdUnit.vue'
+
 export default{
     props: ['element', 'fotoscapeObj'],
     components:{
-        FotoscapeBlock
+        FotoscapeBlock,
+        PreviewBlockHeader,
+        PreviewAdUnit,
     }
 }
 
