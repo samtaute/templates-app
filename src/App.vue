de10438d0b3ded84d6aa30acf196a1bc7dc1a223
sam-taute <sam.taute@digitalturbine.com>
2023-02-08 10:57:00 -0500

    Update platform button colors

--- src/App.vue
+++ src/App.vue
@@ -63,31 +63,225 @@ body {
   z-index:9999; 
 }
 
-.cricket {
-  background-color: aliceblue !important;
+.cricket-button {
+  background-color: #C1CFDA !important;
 }
 
-.blu {
-  background-color: aqua !important;
+.blu-button {
+  background-color: #016FB9 !important;
 }
 
-.boost {
-  background-color: beige !important;
+.boost-button {
+  background-color: #22AED1 !important;
 }
 
-.firstly {
-  background-color: #8E402A !important
+.firstly-button {
+  background-color: #6D8EA0 !important
 }
 
-.metropcs {
-  background-color: #9C9C9C !important;
+.metropcs-button {
+  background-color: #941C2F !important;
 }
 
-.pwg {
-  background-color: #00BB2D !important;
+.pwg-button {
+  background-color: #4A4E69 !important;
 }
 
 .moment-button {
-  background-color: #7E7B52 !important;
+  background-color: #9A8C98 !important;
 }
+.amxbar-button{
+  background-color: #250902 !important;
+}
+
+.attbar-button{
+  background-color: #59F8E8 !important;
+
+}
+.attmxbar-button{
+  background-color: #640D14 !important;
+
+}
+
+.cricketbar-button{
+  background-color: #AD2831 !important; 
+
+}
+
+.firstlybar-button{
+  background-color: #F1FFE7 !important;
+
+}
+.gioneebar-button{
+  background-color: #A9FDAC !important; 
+
+}
+.lenovabar-button{
+  background-color: #44CF6C !important; 
+
+}
+
+.myphonebar-button{
+  background-color: #32A287 !important;
+
+}
+.pwgbar-button{
+  background-color: #6C464E !important;
+
+}
+
+.samsungbrbar-button{
+  background-color: #32A287 !important; 
+
+}
+
+.tracfonebar-button{
+  background-color: #F9DBBD !important; 
+
+}
+.verizonbar-button{
+  background-color: #FFA5AB !important; 
+
+
+}
+.uscellbar-button{
+  background-color: #450920 !important; 
+
+}
+
+.tracfonebar-button{
+  background-color: #CEE7E6 !important; 
+
+}
+
+.verizonbar-button{
+  background-color: #BFC0C0 !important;
+
+
+}
+
+.metrobar-button{
+  background-color: #648767 !important; 
+
+}
+.nokiainbar-button{
+  background-color: #DECDF5 !important; 
+
+
+}
+.timbar-button{
+  background-color: #534D56 !important; 
+
+}
+.aonebar-button{
+  background-color: #5D2A42 !important; 
+
+}
+.nokialatambar-button{
+  background-color: #FB6376 !important; 
+
+
+}
+.nokiamenabar-button{
+  background-color: #FCB1A6 !important
+}
+.cricketminus1-button{
+  background-color: #FFC800 !important;
+}
+.amxweb-button{
+  background-color: #423B0B !important;
+}
+
+.attweb{
+  background-color: #D4DF9E !important;
+
+}
+
+.awweb{
+  background-color:#12100E !important; 
+
+}
+
+.boostweb{
+  background-color: #D3B88C !important; 
+
+}
+.cricketweb{
+  background-color: #D8CC34 !important;
+
+}
+.aoneweb{
+  background-color: #E5B769 !important;
+
+}
+
+.magentaweb{
+  background-color: magenta !important; 
+
+}
+
+.metroweb{
+  background-color: #EDC9FF !important; 
+
+}
+
+.mmxweb{
+  background-color: #F2B79F !important;
+
+}
+
+.myphoneweb{
+  background-color: #5995ED !important; 
+
+}
+
+.nokiainweb{
+  background-color: #FCFF4B !important
+
+}
+
+
+.nokiamenaweb{
+  background-color: #044389 !important; 
+
+}
+
+.orangeweb{
+  background-color: #6CAE75 !important;
+
+}
+
+.pwgweb{
+  background-color: #C1CC99 !important
+
+}
+
+.sprintweb{
+  background-color: #7CAFC4 !important; 
+
+}
+
+.uscellweb{
+  background-color: #F5A65B !important; 
+
+}
+
+.bluweb{
+  background-color: #582C4D !important
+
+}
+
+.verizonweb{
+  background-color: #D5B9B2 !important; 
+
+
+}
+
+.virginweb{
+  background-color: #ECE2D0 !important;
+
+
+}
+
+
 </style>
