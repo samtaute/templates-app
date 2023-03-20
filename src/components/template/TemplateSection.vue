87be07f3c5e7e58f2f1d8d75ae48cb5dae512d26
sam-taute <sam.taute@digitalturbine.com>
2023-03-20 13:29:42 -0400

    Create TemplateSection skeleton

---
+++ src/components/template/TemplateSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <template-block v-for="block in element.blocks"></template-block>
+        
+
+
+    </section>
+
+</template>
+
+<script setup>
+import TemplateBlock from './TemplateBlock.vue'
+
+
+</script>
+
+<style scoped>
+
+
+</style>
