3497cf230f66c564b34ee52dc512262f42692594
sam-taute <sam.taute@digitalturbine.com>
2023-03-20 13:30:55 -0400

    Add sectionblock and exeriment block to models

--- src/models/blockModels.js
+++ src/models/blockModels.js
@@ -1,5 +1,5 @@
-export default{
-    fotoscape_block:{
+export default {
+    fotoscape_block: {
         "blockType": "fotoscape_block",
         "settings": {
             "layout": "edge-description",
@@ -7,5 +7,51 @@ export default{
             "category": "news",
         }
     },
-    
+    section_block: {
+        "blockType": "section_block",
+        "blocks": [
+            {
+                "blockType": "fotoscape_block",
+                "settings": {
+                    "layout": "edge-description",
+                    "count": 1,
+                    "category": "news",
+                }
+            },
+            {
+                "blockType": "fotoscape_block",
+                "settings": {
+                    "layout": "edge-description",
+                    "count": 1,
+                    "category": "news",
+                }
+            }
+
+        ],
+    },
+    experiment_block: {
+        "blockType": "experiment_block",
+        "settings": {
+            "id": "1037851",
+            "variantPercent": 50,
+            "control": {
+                "blockType": "revcontent_block",
+                "settings": {
+                    "widget_id": 268934,
+                    "send_view": true,
+                    "sponsored_count": 2,
+                    "layout": "edge"
+                }
+            },
+            "variant": {
+                "blockType": "revcontent_block",
+                "settings": {
+                    "widget_id": 268936,
+                    "send_view": true,
+                    "sponsored_count": 2,
+                    "layout": "edge"
+                }
+            }
+        }
+    },
 }
