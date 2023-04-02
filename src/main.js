c5adfaa02538a371cc828bdda1f2d2e72313a1b8
sam-taute <sam.taute@digitalturbine.com>
2023-04-02 11:39:16 -0400

    Registers TemplateItemProperty globally

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store/index.js'
 import "bootstrap/dist/js/bootstrap.js";
 import BaseSidebarWidget from './components/ui/BaseSidebarWidget'
 import TemplateBlock from './components/template/TemplateBlock'
+import TemplateItemProperty from './components/refactoredtemplate/TemplateItemProperty'
 
 const app = createApp(App); 
 
@@ -12,6 +13,7 @@ const app = createApp(App);
 app
 .component('BaseSidebarWidget', BaseSidebarWidget)
 .component('TemplateBlock', TemplateBlock)
+.component('TemplateItemProperty',TemplateItemProperty)
 
 app.use(store);
 app.mount('#app');
